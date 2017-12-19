070a61ef4a774401d73f64800223debf36668a5d
kmine <k.minemura27@gmail.com>
2017-12-19 13:09:02 +0900

    change pipeline

--- ci/script/unit.sh
+++ ci/script/unit.sh
@@ -7,4 +7,14 @@ cd repo
 rm -rf ~/.gradle
 ln -fs $(pwd)/gradleCache ~/.gradle
 
+set +e
 ./gradlew --no-daemon test
+
+if [ ! $? = 0 ]; then
+#  tar cvfz artifact.tar.gz build
+#  mv artifact.tar.gz ../unit-result
+  echo "test failed." > ../unit-result/message.txt
+  exit 1
+fi
+
+set -e
